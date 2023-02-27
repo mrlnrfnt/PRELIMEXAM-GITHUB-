0cc2fa77deb51eaaf92c7d36b2f67c08eb346ead
NetAcad DEVASC <changeme@example.com>
2023-02-27 10:07:31 +0000

    PRELIMEXAM GITHUB

---
+++ Conversion.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.case import TestCase
+
+    def conversion (farenheit, celsius):
+
+        return farenheit = celsius 
+
+class Testconversion(unittest.TestCase):
+
+    def test(self):
+        assert 
+
+if __name__=='__main__':
+    unittest.main()
+
+    
